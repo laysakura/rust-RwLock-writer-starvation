c2b7beb868287472ef4411ce01a3947fd149bc2d
Sho Nakatani <lay.sakura@gmail.com>
2022-03-30 15:07:52 +0900

    30 threads

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,17 @@ fn writer_exit(lock: &RwLock<()>) {
 }
 
 fn main() {
-    let r_lock = Arc::new(RwLock::new(()));
-    let w_lock = r_lock.clone();
+    let w_lock = Arc::new(RwLock::new(()));
 
-    let r_handle = thread::spawn(move || {
-        reader_loop(r_lock.as_ref());
-    });
+    for _ in 0..30 {
+        // more than the number of physical CPU cores
+        let r_lock = w_lock.clone();
+        let _r_handle = thread::spawn(move || reader_loop(&r_lock));
+    }
 
     let w_handle = thread::spawn(move || {
         writer_exit(w_lock.as_ref());
     });
 
-    r_handle.join().unwrap();
     w_handle.join().unwrap();
 }
